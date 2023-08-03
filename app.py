590cf45abf35f484e81c558316f97f348e9a6893
AbhiDroid <abhinabroy2001@gmail.com>
2023-08-04 03:34:06 +0530

    add requirements.txt file

--- app.py
+++ app.py
@@ -9,44 +9,15 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# X_test = [[ 0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0    ,0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0  ,  0 ,   0 ,   0 ,   0,
-#      0   , 0   , 0   , 0   , 0    ,0   , 0   , 0   , 0 , 152, 2264  ,152 ,2192 , 415,
-#   6367 , 200 , 407 ,   5 ,6977 ,  73 ,  38 ,3535 ,2195 , 122 ,  71 ,1227 ,4548 ,5119,
-#   7462   ,37 ,1435, 2266 ,3478, 3357 ,2258, 4977,  748  ,  5 ,1349 ,1605]]
-
-# Load the model
-def load_model():
-    with open('review_sentiment_analysis_model.pkl', 'rb') as file:
-        model = pickle.load(file)
-    return model
 
 
 @app.route('/predict', methods=['POST'])
 def post_text_sentiment():
-    # model = load_model()
-    model = tf.keras.models.load_model("sentiment_analysis_model.h5")
 
-    # Y_pred = model.predict(X_test)
-    # print(np.argmax(Y_pred, axis=1))
+    model = tf.keras.models.load_model("sentiment_analysis_model.h5")
 
     with open('tokenizer.pkl', 'rb') as f:
         tokenizer = pickle.load(f)
-
-
 
     data = request.json
     text = data.get("text")
